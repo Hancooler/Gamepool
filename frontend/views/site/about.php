922aff3d47dcfbdfcf273c12daada21a3ac4fdae
Hancooler <187006072@student.unsil.ac.id>
2020-12-30 22:38:19 +0700

    about konten

--- frontend/views/site/about.php
+++ frontend/views/site/about.php
@@ -4,13 +4,16 @@
 
 use yii\helpers\Html;
 
-$this->title = 'About';
+$this->title = 'Tentang';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>This is the About page. You may modify the following file to customize its content:</p>
+    <p>Game Pool merupakan Situs Website yang dibuat agar pengguna bisa mendapatkan informasi mengenai dunia teknologi dan game 
+        <br>
+        GamePool juga memiliki Mini game yang masih dalam Tahap penggembangan yang bisa di mainkan secara gratis
+    </p>
 
-    <code><?= __FILE__ ?></code>
+
 </div>
