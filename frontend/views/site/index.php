43a4dd790a899afd17d433ae3dc50a533e987286
Hancooler <187006072@student.unsil.ac.id>
2021-01-03 23:01:13 +0700

    fix index

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -40,7 +40,7 @@ $this->title = 'My Yii Application';
                     <hr class="section-heading-spacer">
                     <div class="clearfix"></div>
                     <h2 class="section-heading">Mini Game For you</h2>
-                    <p class="lead">Mari di coba <a target="_blank" href=""></a>mini game ini bisa dimainkan </p> 
+                    <p class="lead">Mari di coba <a target="_blank" href=""></a>mini game ini bisa dimainkan game yang tersedia merupakan game sederhana yang masih dalam tahap pengembangan  </p> 
                 </div>
                 <div class="col-lg-5 col-lg-offset-2 col-sm-6">
                     <img class="img-responsive" src="https://image.freepik.com/free-vector/set-virtual-reality-equipment-elements_23-2148812130.jpg" alt="">
@@ -62,7 +62,7 @@ $this->title = 'My Yii Application';
                     <hr class="section-heading-spacer">
                     <div class="clearfix"></div>
                     <h2 class="section-heading">NEWS by Tim Creator</h2>
-                    <p class="lead">Berita ini berisi konten tentang seputar teknologi dan game <a target="_blank" href="">PSDCovers</a>! Tidak ada unsur politik </p>
+                    <p class="lead">Berita ini berisi konten tentang seputar teknologi dan game informasi yang ditampilkan disajikan dalam bentuk sebuah artikel atau buletin <a target="_blank" href="index.php?r=news%2Findex">News</a> Tidak ada unsur politik </p>
                 </div>
                 <div class="col-lg-5 col-sm-pull-6  col-sm-6">
                     <img class="img-responsive" src="https://image.freepik.com/free-vector/people-using-virtual-reality-glasses_23-2148770144.jpg" alt="">
