f36df6a9426c1ef14af23422883c6e44720b791e
Achmad Mahathir Prasanjaya <187006041@student.unsil.ac.id>
2021-01-17 18:55:21 +0700

    Revert to previous fix

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -13,10 +13,9 @@ $this->title = 'My Yii Application';
             <div class="row">
                 <div class="col-lg-12">
                     <div class="intro-message">
-                        <h1>Selamat Datang </h1>
-                        <h3>GamePool Center</h3>
+                        <h1><strong>Selamat Datang</strong></h1>
+                        <h3> GamePool Center</h3>
                         
-                    
                         </ul>
                     </div>
                 </div>
@@ -62,7 +61,7 @@ $this->title = 'My Yii Application';
                     <hr class="section-heading-spacer">
                     <div class="clearfix"></div>
                     <h2 class="section-heading">NEWS by Tim Creator</h2>
-                    <p class="lead">Berita ini berisi konten tentang seputar teknologi dan game informasi yang ditampilkan disajikan dalam bentuk sebuah artikel atau buletin <a target="_blank" href="index.php?r=news%2Findex">News</a> Tidak ada unsur politik. </p>
+                    <p class="lead">Berita ini berisi konten tentang seputar teknologi dan game informasi yang ditampilkan disajikan dalam bentuk sebuah artikel atau buletin <a target="_blank" href="index.php?r=news%2Findex">News</a> Tidak ada unsur politik </p>
                 </div>
                 <div class="col-lg-5 col-sm-pull-6  col-sm-6">
                     <img class="img-responsive" src="https://image.freepik.com/free-vector/people-using-virtual-reality-glasses_23-2148770144.jpg" alt="">
@@ -84,7 +83,8 @@ $this->title = 'My Yii Application';
                     <hr class="section-heading-spacer">
                     <div class="clearfix"></div>
                     <h2 class="section-heading">Tentang Gamepool™</h2>
-                    <p class="lead">Untuk mengenal lebih jauh silahkan klik <a target="_blank" href="index.php?r=site%2Fabout">link</a> ini.</p>
+                    <p class="lead">Untuk mengenal lebih jauh silahkan klik <a target="_blank" href="index.php?r=site%2Fabout">link</a>ini.</p>
+                    <p class="lead">Ingin selalu up to date? Subscribe di <a target="_blank" href="index.php?r=site%2Fabout">Newsletter</a> kami.</p>
                 </div>
                 <div class="col-lg-5 col-lg-offset-2 col-sm-6">
                     <img class="img-responsive" src="img/phones.jpg" alt="">
@@ -97,24 +97,6 @@ $this->title = 'My Yii Application';
     </div>
     <!-- /.content-section-a -->
 
-    <div class="content-section-b">
-
-        <div class="container">
-
-            <div class="row">
-                <div class="col-lg-5 col-lg-offset-1 col-sm-push-6  col-sm-6">
-                    <hr class="section-heading-spacer">
-                    <div class="clearfix"></div>
-                    <h2 class="section-heading">Ingin selalu Up-to-date?</h2>
-                    <p class="lead">Ingin selalu up to date? Subscribe di <a target="_blank" href="https://rocky-scrubland-84244.herokuapp.com">Newsletter</a> kami.</p>
-                </div>
-            </div>
-
-        </div>
-        <!-- /.container -->
-
-    </div>
-    <!-- /.content-section-b -->
 	
    
 </div>
